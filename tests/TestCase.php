0cbb6a1fa3622322f965c60cf449efd7db686cbd
José Ignacio Amelivia Santiago <ohcan2@gmail.com>
2019-05-24 10:43:26 +0200

    Creating a php project structure, with composer and phpunit

---
+++ tests/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Namelivia\Fitbit\Tests;
+
+/**
+ * This is the root test class. All tests will extend from here.
+ *
+ * @author José Ignacio Amelivia Santiago <jignacio.amelivia@gmail.com>
+ */
+class TestCase extends PHPUnit_Framework_TestCase
+{
+	//
+}
