fadb85f72b2c855b52a8768481c3b8d6a918756b
José Ignacio Amelivia Santiago <ohcan2@gmail.com>
2019-07-31 13:29:25 +0200

    Sleep goals operations

--- src/Api/SleepLogs.php
+++ src/Api/SleepLogs.php
@@ -5,18 +5,26 @@ declare(strict_types=1);
 namespace Namelivia\Fitbit\Api;
 
 use Namelivia\Fitbit\SleepLogs\SleepLogs as SleepLogsOperations;
+use Namelivia\Fitbit\SleepLogs\Goals;
 
 class SleepLogs
 {
     private $sleepLogs;
+    private $goals;
 
     public function __construct(Fitbit $fitbit)
     {
         $this->sleepLogs = new SleepLogsOperations($fitbit);
+        $this->goals = new Goals($fitbit);
     }
 
     public function sleepLogs()
     {
         return $this->sleepLogs;
+    }
+
+    public function goals()
+    {
+        return $this->goals;
     }
 }
